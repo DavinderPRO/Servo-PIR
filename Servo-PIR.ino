cb00167a702be86c114faca2a8e8b6a1594e4a22
davinderxav <davindersingharora@gmail.com>
2021-07-29 02:58:53 +0530

    disable serial port messages

--- Servo-PIR.ino
+++ Servo-PIR.ino
@@ -1,10 +1,10 @@
 #include <Servo.h>
 Servo myservo; // create servo object to control a servo
 
-#define led (uint8_t)13           // the pin that the LED is atteched to
-#define pirSensor (uint8_t)2      // the pin that the pirSensor is atteched to
-#define servoPin (uint8_t)9       // the pin that the pirSensor is atteched to
-#define stepper_speed (uint8_t)15 // the pin that the pirSensor is atteched to
+#define led (uint8_t)13      // the pin that the LED is atteched to
+#define pirSensor (uint8_t)2 // the pin that the pirSensor is atteched to
+#define servoPin (uint8_t)9  // the pin that the pirSensor is atteched to
+//#define stepper_speed (uint8_t)15 // the pin that the pirSensor is atteched to
 
 int sensorValue = 0;  // variable to store the pirSensor status (value)
 uint8_t position = 0; // variable to store the servo position
@@ -16,7 +16,7 @@ void setup()
   pinMode(pirSensor, INPUT); // initialize pirSensor as an input
 
   myservo.attach(servoPin); // attaches the servo on pin 9 to the servo object
-  Serial.begin(9600);       // initialize serial
+                            // Serial.begin(9600);       // initialize serial
 }
 
 void setServoMotor(bool state)
@@ -26,7 +26,7 @@ void setServoMotor(bool state)
     for (position = 0; position <= 180; position += 1) // goes from 0 degrees to 180 degrees
     {
       myservo.write(position); // tell servo to go to position in variable 'position'
-      delay(stepper_speed);
+      delay(15);
     }
   }
   else
@@ -34,7 +34,7 @@ void setServoMotor(bool state)
     for (position = 180; position >= 0; position -= 1) // goes from 180 degrees to 0 degrees
     {
       myservo.write(position); // tell servo to go to position in variable 'position'
-      delay(stepper_speed);
+      delay(15);
     }
   }
 }
@@ -46,10 +46,11 @@ void loop()
   if (sensorValue == HIGH)
   {
     digitalWrite(led, HIGH); // turn LED ON
-    delay(500);              // delay 100 milliseconds
+    delay(100);              // delay 100 milliseconds
+
     if (state == LOW)
     {
-      Serial.println("Motion detected!");
+      // Serial.println("Motion detected!");
       state = HIGH; // update variable state to HIGH
       setServoMotor(state);
     }
@@ -57,10 +58,10 @@ void loop()
   else
   {
     digitalWrite(led, LOW); // turn LED OFF
-    delay(500);             // delay 200 milliseconds
+    delay(200);             // delay 200 milliseconds
     if (state == HIGH)
     {
-      Serial.println("Motion stopped!");
+      // Serial.println("Motion stopped!");
       state = LOW; // update variable state to LOW
       setServoMotor(state);
     }
