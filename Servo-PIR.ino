851f4c59e243f7804be2f01c5a7addd12a21c531
davinderxav <davindersingharora@gmail.com>
2021-08-02 11:41:51 +0530

    Added mp3 player

--- Servo-PIR.ino
+++ Servo-PIR.ino
@@ -1,10 +1,23 @@
+#include "Arduino.h"
+#include "DFPlayerMini_Fast.h"
+#include "SoftwareSerial.h"
 #include <Servo.h>
-Servo myservo; // create servo object to control a servo
 
-#define led (uint8_t)13           // the pin that the LED is atteched to
-#define pirSensor (uint8_t)2      // the pin that the pirSensor is atteched to
-#define servoPin (uint8_t)9       // the pin that the pirSensor is atteched to
-#define stepper_speed (uint8_t)15 // the pin that the pirSensor is atteched to
+#define led (uint8_t)13      // the pin that the LED is atteched to
+#define pirSensor (uint8_t)8 // the pin that the pirSensor is atteched to
+#define servoPin (uint8_t)9  // SHould be digital, Prefer 9,10
+
+#define mp3Tx (uint8_t)6 // to mp3 rx
+#define mp3Rx (uint8_t)7 // to mp3 tx
+
+#define servoSpeed (uint8_t)15    // the pin that the pirSensor is atteched to
+#define EQ_Preset (uint8_t)2      //Rock
+#define defaultVolume (uint8_t)25 // to mp3 tx
+#define fileToPlay (uint16_t)2    // to mp3 tx
+
+Servo servo;                             // create servo object to control a servo
+SoftwareSerial softSerial(mp3Rx, mp3Tx); // RX, TX
+DFPlayerMini_Fast myDFPlayer;
 
 uint8_t position = 0; // variable to store the servo position
 bool state = false;   // by default, no motion detected
@@ -13,38 +26,65 @@ void setup()
 {
   pinMode(led, OUTPUT);      // initalize LED as an output
   pinMode(pirSensor, INPUT); // initialize pirSensor as an input
-  myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
-  myservo.write(position);
-  delay(2000); // Give motor 2 second delay to reach 0 postion on start up
-  // Serial.begin(9600); // initialize serial
+  servo.attach(servoPin);    // attaches the servo on pin 9 to the servo object
+  servo.write(position);
+  softSerial.begin(9600);
+  Serial.begin(9600); // initialize serial
+  if (!myDFPlayer.begin(softSerial, true))
+  {
+    Serial.println("Unable to begin:");
+    Serial.println("1.Please recheck the connection!");
+    Serial.println("2.Please insert the SD card!");
+
+    while (true)
+    {
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
+  Serial.println("DFPlayer Mini online.");
+  myDFPlayer.EQSelect(EQ_Preset);
+  myDFPlayer.volume(defaultVolume); //Set volume value. From 0 to 30
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(1000); // Give motor 2 second delay to reach 0 postion on start up
 }
-// inline void writeServoPosition()
-// {
-//   myservo.write(position);
-//   delay(stepper_speed);
-// }
+
+inline void writeServoPosition()
+{
+  servo.write(position);
+  delay(servoSpeed);
+}
+
 void loop()
 {
   if ((!state && digitalRead(pirSensor) == HIGH) || (state && digitalRead(pirSensor) == LOW))
   {
     state = !state;
-    digitalWrite(led, state);       // Flip LED
-    while (state && position < 180) // goes from 0 degrees to 180 degrees
+    digitalWrite(led, state); // Flip LED
+    if (state)
     {
-      position++;
-      // Serial.println(position);
-      // writeServoPosition();
-      myservo.write(position);
-      delay(stepper_speed);
+      myDFPlayer.wakeUp();
+      Serial.println(("sending MP3 loop"));
+      myDFPlayer.loop(fileToPlay);
+      while (position < 180) // goes from 0 degrees to 180 degrees
+      {
+        position++;
+        Serial.println(position);
+        writeServoPosition();
+      }
     }
-    while (!state && position > 0) // goes from 180 degrees to 0 degrees
+    else
     {
-      position--;
-      // Serial.println(position);
-      // writeServoPosition();
-      myservo.write(position);
-      delay(stepper_speed);
+      while (position > 0) // goes from 180 degrees to 0 degrees
+      {
+        position--;
+        Serial.println(position);
+        writeServoPosition();
+      }
+      Serial.println(("Stopping mp3"));
+      myDFPlayer.stop();
+      myDFPlayer.sleep();
     }
   }
-  delay(2);
 }
